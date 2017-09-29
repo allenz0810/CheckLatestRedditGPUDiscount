41de5959f89a7125452805a3f54624ab0d3ddffb
Allenz0810 <lingjiez321@gmail.com>
2017-09-29 09:59:52 -0400

    Import project

--- CheckLatestRedditGUPDiscount/CheckLatestRedditGUPDiscount/Program.cs
+++ CheckLatestRedditGUPDiscount/CheckLatestRedditGUPDiscount/Program.cs
@@ -1,26 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 
 namespace CheckLatestRedditGUPDiscount
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
+            Console.WriteLine("Hello World!");
         }
     }
 }
